c55dd85c05ec47f75a2c72b7a05b18272ed705e5
Hafis20 <hafisvh2002@gmail.com>
2024-05-23 16:48:12 +0530

    [4] team-result completed

--- controllers/teamResultController.js
+++ controllers/teamResultController.js
@@ -1,10 +1,24 @@
-
+const Team = require('../model/teamModel');
 
 const teamResult = async (req, res) => {
     try {
-        res.status(200).json('Team result');
+        const allTeams = await Team.find({})
+        const maxPoints = Math.max(...allTeams.map(team => team.points));
+        const winners = allTeams.filter(team => team.points === maxPoints);
+        console.log(winners);
+        res.json({
+            allTeams: allTeams.map(team => ({
+                name: team.teamName,
+                totalPoints: team.points
+            })),
+            winners: winners.map(team => ({
+                name: team.teamName,
+                totalPoints: team.points
+            }))
+        });
+
     } catch (error) {
-        res.status(500).json({message:'Internal Server error'});
+        res.status(500).json({ message: 'Internal Server error' });
     }
 }
 
